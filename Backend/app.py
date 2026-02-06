2624f7d2493393a6c6bd7cd906be22febcf24464
Vishva-2907 <vishvashah2907@gmail.com>
2026-02-06 11:26:55 +0530

    Update login.html

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,21 @@
-print("Hello World")
+from flask import Flask, render_template
+ 
+app = Flask(
+
+    __name__,
+
+    template_folder="../Frontend/templates",
+
+    static_folder="../Frontend/static"
+
+)
+ 
+@app.route("/")
+
+def home():
+
+    return render_template("login.html")  # or "index.html"
+ 
+if __name__ == "__main__":
+
+    app.run(debug=True)
